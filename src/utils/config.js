bc8cc00cfc124fcbf321f8fdfd1d3dd0cb347555
ximolang-PC <txiliang@163.com>
2018-02-28 16:43:36 +0800

    增加记账页面

---
+++ src/utils/config.js
@@ -0,0 +1,207 @@
+// 账本支出类目
+export const payCateList = [
+  {
+    type: 'pay',
+    cateId: 1,
+    cateName: '餐饮'
+  },
+  {
+    type: 'pay',
+    cateId: 2,
+    cateName: '购物'
+  },
+  {
+    type: 'pay',
+    cateId: 3,
+    cateName: '日用'
+  },
+  {
+    type: 'pay',
+    cateId: 4,
+    cateName: '交通'
+  },
+  {
+    type: 'pay',
+    cateId: 5,
+    cateName: '蔬菜'
+  },
+  {
+    type: 'pay',
+    cateId: 6,
+    cateName: '水果'
+  },
+  {
+    type: 'pay',
+    cateId: 7,
+    cateName: '零食'
+  },
+  {
+    type: 'pay',
+    cateId: 8,
+    cateName: '运动'
+  },
+  {
+    type: 'pay',
+    cateId: 9,
+    cateName: '娱乐'
+  },
+  {
+    type: 'pay',
+    cateId: 10,
+    cateName: '通讯'
+  },
+  {
+    type: 'pay',
+    cateId: 11,
+    cateName: '服饰'
+  },
+  {
+    type: 'pay',
+    cateId: 12,
+    cateName: '美容'
+  },
+  {
+    type: 'pay',
+    cateId: 13,
+    cateName: '住房'
+  },
+  {
+    type: 'pay',
+    cateId: 14,
+    cateName: '居家'
+  },
+  {
+    type: 'pay',
+    cateId: 15,
+    cateName: '孩子'
+  },
+  {
+    type: 'pay',
+    cateId: 16,
+    cateName: '长辈'
+  },
+  {
+    type: 'pay',
+    cateId: 17,
+    cateName: '社交'
+  },
+  {
+    type: 'pay',
+    cateId: 18,
+    cateName: '旅行'
+  },
+  {
+    type: 'pay',
+    cateId: 19,
+    cateName: '烟酒'
+  },
+  {
+    type: 'pay',
+    cateId: 20,
+    cateName: '数码'
+  },
+  {
+    type: 'pay',
+    cateId: 21,
+    cateName: '汽车'
+  },
+  {
+    type: 'pay',
+    cateId: 22,
+    cateName: '医疗'
+  },
+  {
+    type: 'pay',
+    cateId: 23,
+    cateName: '书籍'
+  },
+  {
+    type: 'pay',
+    cateId: 24,
+    cateName: '学习'
+  },
+  {
+    type: 'pay',
+    cateId: 25,
+    cateName: '宠物'
+  },
+  {
+    type: 'pay',
+    cateId: 26,
+    cateName: '礼金'
+  },
+  {
+    type: 'pay',
+    cateId: 27,
+    cateName: '礼物'
+  },
+  {
+    type: 'pay',
+    cateId: 28,
+    cateName: '办公'
+  },
+  {
+    type: 'pay',
+    cateId: 29,
+    cateName: '维修'
+  },
+  {
+    type: 'pay',
+    cateId: 30,
+    cateName: '捐赠'
+  },
+  {
+    type: 'pay',
+    cateId: 31,
+    cateName: '彩票'
+  },
+  {
+    type: 'pay',
+    cateId: 32,
+    cateName: '亲友'
+  },
+  {
+    type: 'pay',
+    cateId: 33,
+    cateName: '快递'
+  },
+  {
+    type: 'pay',
+    cateId: 34,
+    cateName: '其他'
+  }
+]
+
+// 账本收入类目
+export const incomeCateList = [
+  {
+    type: 'income',
+    cateId: 1,
+    cateName: '工资'
+  },
+  {
+    type: 'income',
+    cateId: 2,
+    cateName: '兼职'
+  },
+  {
+    type: 'income',
+    cateId: 3,
+    cateName: '理财'
+  },
+  {
+    type: 'income',
+    cateId: 4,
+    cateName: '礼金'
+  },
+  {
+    type: 'income',
+    cateId: 5,
+    cateName: '红包'
+  },
+  {
+    type: 'income',
+    cateId: 6,
+    cateName: '其他'
+  }
+]
